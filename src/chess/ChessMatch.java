94f5dbede934434d4679b14ff21b37212f88df52
Pedro Cunha <idk.pedroc@gmail.com>
2022-12-28 03:52:50 -0300

    Possible moves to Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,6 +34,14 @@ public class ChessMatch {
 		}
 	}
 	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to chosen position");
+		}
+	}
+	
+	
+	
 	private Piece makeMove(Position source, Position target) {	
 		
 		Piece p = board.removePiece(source);
@@ -47,6 +55,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
